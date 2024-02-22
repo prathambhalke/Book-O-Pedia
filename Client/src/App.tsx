792b5eb3ad0288c56f9a7a5e20115511a43030fe
prathambhalke <prathameshbhalke125@gmail.com>
2024-02-22 09:47:32 +0530

    Client side folder setup

---
+++ Client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold underline text-red-500">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
