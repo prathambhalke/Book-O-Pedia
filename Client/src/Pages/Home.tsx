81404d7f75f5071c17a17aa4f14e9df01c50222a
prathambhalke <prathameshbhalke125@gmail.com>
2024-02-26 11:41:12 +0530

    updated the create book func

--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import Table from "../Components/Table";
 import { BiSolidBookAdd } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import { Loader } from "../..";
+import { CreateBook, Loader } from "../..";
+import classnames from "tailwindcss-classnames";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const [bookCount, setbookCount] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -19,30 +21,44 @@ const Home = () => {
         setbookCount(res.data.count);
         setLoading(false);
       })
-      .catch((err: any) => {
+      .catch((err) => {
         setLoading(false);
         console.log(err);
       });
   }, []);
+
+  const toggleCreateForm = () => {
+    setShowCreateForm(!showCreateForm);
+  };
+
   return (
-    <div className="w-[85vw] flex flex-col relative">
+    <div className={`w-[85vw] flex flex-col relative`}>
       <div className="flex justify-between items-center gap-4 py-6 border-b border-gray-200">
-        <h1 className="text-2xl font-bold">Books Store</h1>
+        <div className="text-2xl font-bold flex items-center">
+          <img src="https://img.icons8.com/color/48/book.png" alt="book logo" />
+          Book-O-Pedia
+        </div>
         <div className="flex items-center gap-4 relative">
-          <Link
-            to="/books/create"
+          <button
+            onClick={toggleCreateForm}
             className="flex items-center text-green-600 relative hover:scale-110 transition-all duration-100'"
           >
             <BiSolidBookAdd className="text-3xl relative" />
             <span className="absolute top-1 right-1 transform translate-x-1/2 -translate-y-1/2 bg-white rounded-full text-[12px] font-medium w-4 h-4 flex items-center justify-center border border-gray-200">
               {bookCount}
             </span>
-          </Link>
+          </button>
         </div>
       </div>
       {loading ? <Loader /> : <Table Tdata={data} />}
+      {showCreateForm && (
+        <div className="fixed top-0 left-0 w-full h-full z-10 flex justify-center items-center backdrop-blur-[2px]">
+          <CreateBook />
+        </div>
+      )}
+
     </div>
-  );;
+  );
 };
 
 export default Home;
